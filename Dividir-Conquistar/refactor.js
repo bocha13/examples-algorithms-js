44d916dc0e1af03438da90cd5e9ba1ac95e1ff92
Bocha13 <ignaciobochatay@gmail.com>
2019-03-06 22:28:10 -0300

    modificacion del dividir-conquistar/refactor.js

--- Dividir-Conquistar/refactor.js
+++ Dividir-Conquistar/refactor.js
@@ -2,9 +2,9 @@
 function search(arr, val) {
   let min = 0;
   let max = arr.length -1;
-  while (min < max) {
+  while (min <= max) {
     let middle = Math.floor((min + max) / 2);
-    let currentElement = array[middle];
+    let currentElement = arr[middle];
     if (arr[middle] < val) {
       min = middle + 1;
     }
@@ -17,3 +17,5 @@ function search(arr, val) {
   }
   return -1;
 }
+
+console.log(search([1,3,4,5,6,7,8,9], 4));
